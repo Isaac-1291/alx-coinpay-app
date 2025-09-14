8bc7183005d9253c309c0e5f4a89d3e1ff7760bd
Isaac-1291 <Profkb91@gmail.com>
2025-09-14 21:21:15 +0000

    alx FE project

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center bg-blue-500">
+      <Text className="text-white text-xl font-bold">Hello CoinPay!</Text>
+    </View>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,ts,tsx}",
+    "./components/**/*.{js,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
